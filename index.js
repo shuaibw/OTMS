c7ec5826d7a05f7a82cd2195aef73449f29ab5a7
shuaibw <1805010@ugrad.cse.buet.ac.bd>
2022-01-19 09:53:18 +0600

    Separate registration router

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const morgan = require('morgan');
 const express = require('express');
 const multer = require('multer');
 const loginRouter = require('./routes/loginRoutes');
-const studentRegRouter = require('./routes/stuRegRoutes');
+const registrationRouter = require('./routes/registrationRoutes');
 
 const app = express();
 app.use(express.json());
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(multer().array());
 app.use(morgan('dev'));
 app.use('/signin', loginRouter);
-app.use('/registerStudent', studentRegRouter);
+app.use('/register', registrationRouter);
 
 app.listen(3000, () => {
   console.log('Server listeing at 3000');
