2a3e1c5e1d07006263c7ed7fdb3490a6abf07091
shuaibw <1805010@ugrad.cse.buet.ac.bd>
2022-02-04 15:59:54 +0600

    Add instructor registration

--- controller/registrationController.js
+++ controller/registrationController.js
@@ -2,27 +2,42 @@ const executeQuery = require('../models/executeQuery');
 const bcrypt = require('bcrypt');
 
 exports.registerIntructor = async (req, res) => {
-    const regData = req.body;
-    regQuery = `INSERT INTO INSTRUCTORS VALUES(NULL, :full_name, :email, :username, :password)`;
+    const query = `INSERT INTO INSTRUCTORS 
+(NAME, LOCATION, EMAIL, USERNAME, PASSWORD, PHONE, AGE, GENDER, INSTITUTION, YEAR, DEPARTMENT)
+    VALUES 
+(:fullname, :location, :email, :username, :password, :phone, :age, :gender, :institution, :year, :department)`;
+    const data = req.body;
+    console.log(data);
     try {
-        const success = await executeQuery(regQuery, [
-            regData.full_name,
-            regData.email,
-            regData.username,
-            regData.password,
+        const hashedPwd = await bcrypt.hash(data.password, 10);
+        const result = await executeQuery(query, [
+            data.fullname,
+            data.location,
+            data.email,
+            data.username,
+            hashedPwd,
+            data.phone,
+            data.age,
+            data.gender,
+            data.institution,
+            data.year,
+            data.department,
         ]);
-        res.status(200).redirect('http://localhost:5501/frontend/login.html');
+        console.log(result);
+        if (result) res.redirect('http://localhost:3000/login');
+        else res.redirect('http://localhost:3000/register');
     } catch (err) {
-        res.status(500).redirect('http://localhost:5501/frontend/newUser.html');
+        console.log(err);
+        res.redirect('http://localhost:3000/register');
     }
 };
 
 exports.registerStudent = async (req, res) => {
-    const query = `INSERT INTO STUDENTS (NAME, LOCATION, EMAIL, USERNAME, PASSWORD, PHONE, AGE, GENDER, SCHOOL, GRADE, FACULTY,
-    VERSION)
-VALUES (:fullname, :location, :email, :username, :password, :phone, :age, :gender, :school,
-:grade, :faculty, :version)`;
-    data = req.body;
+    const query = `INSERT INTO STUDENTS 
+(NAME, LOCATION, EMAIL, USERNAME, PASSWORD, PHONE, AGE, GENDER, SCHOOL, GRADE, FACULTY, VERSION)
+    VALUES 
+(:fullname, :location, :email, :username, :password, :phone, :age, :gender, :school, :grade, :faculty, :version)`;
+    const data = req.body;
     try {
         const hashedPwd = await bcrypt.hash(data.password, 10);
         const result = await executeQuery(query, [
