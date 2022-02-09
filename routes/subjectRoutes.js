582f326ab3239e90f6aac0e2cbcbacf99e487af6
shuaibw <1805010@ugrad.cse.buet.ac.bd>
2022-02-09 09:48:50 +0600

    Fix subject case, home title, instructor list nav bar title

--- routes/subjectRoutes.js
+++ routes/subjectRoutes.js
@@ -4,15 +4,21 @@ const checkAuth = require('../controller/checkAuth');
 const path = require('path');
 const executeQuery = require('../models/executeQuery');
 router.route('/:subject/instructors').get(checkAuth.is, checkAuth.isStudent, async (req, res) => {
-    const subject = req.params.subject;
+    let subject = req.params.subject;
     const query = `SELECT NAME, INSTITUTION, DEPARTMENT, YEAR, EMAIL,PHONE
     FROM INSTRUCTORS I,
          TEACHES T
     WHERE I.ID = T.INSTRUCTOR_ID and T.SUBJECT_NAME = :subject`;
     const bind = { subject: subject };
     const result = await executeQuery(query, bind, {});
-    console.log(result.rows);
-    res.render(path.resolve('frontend/list-instructors-bysubjects.ejs'), { instructors: result.rows });
+    // console.log(result.rows);
+    if (subject === 'ict') subject = 'ICT';
+    else subject = subject.charAt(0).toUpperCase() + subject.slice(1);
+    res.render(path.resolve('frontend/list-instructors-bysubjects.ejs'), {
+        instructors: result.rows,
+        title: `Meet our ${subject} instructors!`,
+        subject: subject,
+    });
 });
 
 module.exports = router;
