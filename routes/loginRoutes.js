6b49eadeffe14f04e3c25e182d8b8c343b5c9748
shuaibw <1805010@ugrad.cse.buet.ac.bd>
2022-02-09 09:01:32 +0600

    Fix login.ejs background

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -12,7 +12,7 @@ router
         // res.sendFile(path.resolve('frontend/login.html'));
         res.render(path.resolve('frontend/login.ejs'), {
             welcomeMsg: 'Welcome Learner!',
-            backgroundGrad: 'background-image: linear-gradient(to right top, #b7f9f8, #b1f1ff, #c0e5ff, #ddd7ff, #f9c8f1);',
+            backgroundGrad: 'linear-gradient(to right top, #b7f9f8, #b1f1ff, #c0e5ff, #ddd7ff, #f9c8f1)',
             askMsg: 'Are you an instructor?',
             // Route to instructor login upon clicking Not a student
             toInstRoute: '/login/instructor',
@@ -33,7 +33,7 @@ router
     .get(checkAuth.not, async (req, res) => {
         res.render(path.resolve('frontend/login.ejs'), {
             welcomeMsg: 'Welcome Instructor!',
-            backgroundGrad: 'background-image: linear-gradient(to left bottom, #ffffff, #e0dafe, #bfb5fb, #9d92f7, #776ff2);',
+            backgroundGrad: 'linear-gradient(to left bottom, #ffffff, #e0dafe, #bfb5fb, #9d92f7, #776ff2)',
             askMsg: 'Are you a student?',
             toInstRoute: '/login',
             // POST to /login/instructor upon clicking login
