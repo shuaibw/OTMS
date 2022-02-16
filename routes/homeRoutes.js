d085fc36bea4ba5c0e24e79c6678f5f844dc1f59
shuaibw <1805010@ugrad.cse.buet.ac.bd>
2022-02-16 19:50:26 +0600

    Complete milestone2

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,15 +1,21 @@
+const { exec } = require('child_process');
 const express = require('express');
 const router = express.Router();
 const path = require('path');
 const checkAuth = require('../controller/checkAuth');
+const executeQuery = require('../models/executeQuery');
 
 router.route('/').get(checkAuth.is, checkAuth.isStudent, async (req, res) => {
     res.render(path.resolve('frontend/home.ejs'));
 });
 
 router.route('/instructor').get(checkAuth.is, checkAuth.isInstructor, async (req, res) => {
-    // console.log(req.session);
-    res.render(path.resolve('frontend/instHome.ejs'), { instName: req.user.USERNAME });
+    // console.log(req.user);
+    const query = `SELECT NAME, LOCATION, EMAIL, USERNAME, PASSWORD, PHONE, AGE, INSTITUTION, DEPARTMENT
+    FROM INSTRUCTORS I WHERE I.ID = :id`;
+    const binds = { id: req.user.ID };
+    const result = await executeQuery(query, binds, {});
+    res.render(path.resolve('frontend/instructor-edit.ejs'), result.rows[0]);
 });
 
 module.exports = router;
